6014231de6ad500be255ab67e8caa4760d73f59a
tanguylaheurte <tanguylaheurte@gmail.com>
2017-02-24 12:58:18 +0100

    Update on the main -> use of an example
    Our main does not work with CoxRossRubinstein_2. The link is not done and we don't understand why. Because we cannot test our binomial tree, we cannot be sure how to change the j parameter in the underlying.
    Stangely, the main works when we choose CoxRossRubinstein but then it uses the the binomialtree of QL installed on the PC.

--- project3/main.cpp
+++ project3/main.cpp
@@ -1,18 +1,120 @@
 
+
 #include "binomialtree.hpp"
 #include "binomialengine.hpp"
-#include <ql/methods/lattices/binomialtree.hpp>
-#include <ql/pricingengines/vanilla/binomialengine.hpp>
-#include <iostream>
+#include <ql/stochasticprocess.hpp>
 
+#include <ql/pricingengines/vanilla/all.hpp>
+#include <iostream>
+#include <ql/time/calendars/target.hpp>
+#include <boost/timer.hpp>
 using namespace QuantLib;
 
 int main() {
 
     try {
+		 
+        std::cout << std::endl;
+		Size timeSteps = 801;
+        // set up dates
+        Calendar calendar = TARGET();
+        Date todaysDate(15, May, 1998);
+        Date settlementDate(18, May, 1998);
+        Settings::instance().evaluationDate() = todaysDate;
 
-        // add your code here
+        // our options
+        Option::Type type(Option::Put);
+        Real underlying = 36;
+        Real strike = 40;
+        Spread dividendYield = 0.00;
+        Rate riskFreeRate = 0.06;
+        Volatility volatility = 0.20;
+        Date maturity(17, May, 1999);
+        DayCounter dayCounter = Actual365Fixed();
 
+        std::cout << "Option type = "  << type << std::endl;
+        std::cout << "Maturity = "        << maturity << std::endl;
+        std::cout << "Underlying price = "        << underlying << std::endl;
+        std::cout << "Strike = "                  << strike << std::endl;
+
+        std::cout << std::endl;
+        std::string method;
+        std::cout << std::endl ;
+
+        // write column headings
+        Size widths[] = { 35, 14, 14, 14 };
+        std::cout << std::setw(widths[0]) << std::left << "Method"
+                  << std::setw(widths[1]) << std::left << "European"
+                  << std::endl;
+
+        std::vector<Date> exerciseDates;
+        for (Integer i=1; i<=4; i++)
+            exerciseDates.push_back(settlementDate + 3*i*Months);
+
+        boost::shared_ptr<Exercise> europeanExercise(
+                                         new EuropeanExercise(maturity));
+		
+		Handle<Quote> underlyingH(
+            boost::shared_ptr<Quote>(new SimpleQuote(underlying)));
+		// bootstrap the yield/dividend/vol curves
+        Handle<YieldTermStructure> flatTermStructure(
+            boost::shared_ptr<YieldTermStructure>(
+                new FlatForward(settlementDate, riskFreeRate, dayCounter)));
+        Handle<YieldTermStructure> flatDividendTS(
+            boost::shared_ptr<YieldTermStructure>(
+                new FlatForward(settlementDate, dividendYield, dayCounter)));
+        Handle<BlackVolTermStructure> flatVolTS(
+            boost::shared_ptr<BlackVolTermStructure>(
+                new BlackConstantVol(settlementDate, calendar, volatility,
+                                     dayCounter)));
+        boost::shared_ptr<StrikedTypePayoff> payoff(
+                                        new PlainVanillaPayoff(type, strike));
+        boost::shared_ptr<BlackScholesMertonProcess> bsmProcess(new BlackScholesMertonProcess(underlyingH, flatDividendTS,
+                                               flatTermStructure, flatVolTS));
+
+        // options
+        VanillaOption europeanOption(payoff, europeanExercise);
+		//B&S=3.844308
+		//OldBinomial=3.843504
+		//newBinomial=3.843431
+ 
+        // Analytic formulas:
+
+        // Black-Scholes for European
+        method = "Black-Scholes";
+        europeanOption.setPricingEngine(boost::shared_ptr<PricingEngine>(new AnalyticEuropeanEngine(bsmProcess)));
+        std::cout << std::setw(widths[0]) << std::left << method
+                  << std::fixed
+                  << std::setw(widths[1]) << std::left << europeanOption.NPV()
+                  << std::endl;
+		
+
+		method = "Binomial Cox-Ross-Rubinstein";
+        europeanOption.setPricingEngine(boost::shared_ptr<PricingEngine>(
+                      new BinomialVanillaEngine_2<CoxRossRubinstein_2>(bsmProcess,
+                                                                   timeSteps)));
+		
+        std::cout << std::setw(widths[0]) << std::left << method
+                  << std::fixed
+                  << std::setw(widths[1]) << std::left << europeanOption.NPV()
+                  << std::endl;
+		//double seconds = timer.elapsed();
+        //Integer hours = int(seconds/3600);
+        //seconds -= hours * 3600;
+        //Integer minutes = int(seconds/60);
+        //seconds -= minutes * 60;
+        /*std::cout << " \nRun completed in ";tion de l'État, la compétitivité des entreprises et les changements sur le marché du travail. Pas de révolutions sur aucun des trois chapitres, mais des évolutions significatives"
+        if (hours > 0)
+            std::cout << hours << " h ";
+        if (hours > 0 || minutes > 0)
+            std::cout << minutes << " m ";
+        std::cout << std::fixed << std::setprecision(0)
+                  << seconds << " s\n" << std::endl;*/
+//affichage du delta et du gamma
+
+  		std::cout << europeanOption.delta() << std::endl;
+		std::cout << europeanOption.gamma() << std::endl;
+		std::cout << "al" << std::endl;
         return 0;
 
     } catch (std::exception& e) {
@@ -23,4 +125,3 @@ int main() {
         return 1;
     }
 }
-
